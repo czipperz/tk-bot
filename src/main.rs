b9a9ee45e24beb9907aef864d6fff14ec7e514a1
Chris Gregory <czipperz@gmail.com>
2019-03-31 01:27:56 -0400

    Rework respond to interpret raw Data type

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use rocket::Data;
 
 #[post("/", data = "<request>")]
-fn respond(request: String) -> String {
-    request
+fn respond(request: Data) -> String {
+    use std::io::Read;
+    let mut text = String::new();
+    request.open().read_to_string(&mut text).unwrap();
+    text
 }
 
 fn main() {
